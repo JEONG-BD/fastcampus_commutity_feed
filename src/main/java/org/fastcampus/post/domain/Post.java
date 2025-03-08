694c3e3ea8f3128562db1986f0bf6f6fd19e69ad
JEONG-BD <dev.byeongdu@gmail.com>
2025-03-08 18:23:18 +0900

    Like

--- src/main/java/org/fastcampus/post/domain/Post.java
+++ src/main/java/org/fastcampus/post/domain/Post.java
@@ -1,18 +1,38 @@
 package org.fastcampus.post.domain;
 
+import org.fastcampus.common.domain.PositiveIntegerCounter;
 import org.fastcampus.post.domain.content.PostContent;
 import org.fastcampus.user.domain.User;
 
 public class Post {
+    private final Long id;
     private final User author;
     private final PostContent content;
+    private final PositiveIntegerCounter likeCount;
 
-    public Post(User author, PostContent content) {
+    public Post(Long id, User author, PostContent content) {
         if(author == null){
             throw new IllegalArgumentException();
         }
-
+        this.id = id;
         this.author = author;
         this.content = content;
+        this.likeCount = new PositiveIntegerCounter();
     }
+
+    public void like(User user){
+        if(this.author.equals(user)){
+            throw new IllegalArgumentException();
+        }
+        likeCount.increase();
+    }
+
+    public void unlike(User user){
+        if(this.author.equals(user)){
+            throw new IllegalArgumentException();
+        }
+        likeCount.decrease();
+    }
+
+
 }

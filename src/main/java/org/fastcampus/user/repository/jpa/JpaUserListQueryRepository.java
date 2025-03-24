3484b1c75d2f22374c082ea94a5cf84dea403f23
JEONG-BD <dev.byeongdu@gmail.com>
2025-03-24 19:00:09 +0900

    JpaUserListQueryRepository 생성

---
+++ src/main/java/org/fastcampus/user/repository/jpa/JpaUserListQueryRepository.java
@@ -0,0 +1,24 @@
+package org.fastcampus.user.repository.jpa;
+
+import org.fastcampus.user.application.dto.GetUserListResponseDto;
+import org.fastcampus.user.repository.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface JpaUserListQueryRepository extends JpaRepository<UserEntity, Long> {
+
+    @Query(value = "SELECT new org.fastcampus.user.application.dto.GetUserListResponseDto(u.name, u.profileImage) "
+            + "FROM UserRelationEntity ur "
+            + "INNER JOIN UserEntity u ON u.followerCount = u.id "
+            + "WHERE ur.followingUserId = :userId")
+    List<GetUserListResponseDto> getFollowingUserList(Long userId);
+
+    @Query(value = "SELECT new org.fastcampus.user.application.dto.GetUserListResponseDto(u.name, u.profileImage) "
+            + "FROM UserRelationEntity ur "
+            + "INNER JOIN UserEntity u ON u.followingCount = u.id "
+            + "WHERE ur.followerUserId = :userId")
+    List<GetUserListResponseDto> getFollowerUserList(Long userId);
+
+}

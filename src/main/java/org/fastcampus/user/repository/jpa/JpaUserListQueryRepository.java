9301101ca8ad025ffefebb0a2cd70101a2e0e45b
JEONG-BD <dev.byeongdu@gmail.com>
2025-03-31 19:33:30 +0900

    follwing follower 수정

--- src/main/java/org/fastcampus/user/repository/jpa/JpaUserListQueryRepository.java
+++ src/main/java/org/fastcampus/user/repository/jpa/JpaUserListQueryRepository.java
@@ -11,13 +11,13 @@ public interface JpaUserListQueryRepository extends JpaRepository<UserEntity, Lo
 
     @Query(value = "SELECT new org.fastcampus.user.application.dto.GetUserListResponseDto(u.name, u.profileImage) "
             + "FROM UserRelationEntity ur "
-            + "INNER JOIN UserEntity u ON u.followerCount = u.id "
+            + "INNER JOIN UserEntity u ON ur.followerUserId = u.id "
             + "WHERE ur.followingUserId = :userId")
     List<GetUserListResponseDto> getFollowingUserList(Long userId);
 
     @Query(value = "SELECT new org.fastcampus.user.application.dto.GetUserListResponseDto(u.name, u.profileImage) "
             + "FROM UserRelationEntity ur "
-            + "INNER JOIN UserEntity u ON u.followingCount = u.id "
+            + "INNER JOIN UserEntity u ON ur.followingUserId = u.id "
             + "WHERE ur.followerUserId = :userId")
     List<GetUserListResponseDto> getFollowerUserList(Long userId);
 
--- src/main/java/org/fastcampus/user/ui/UserController.java
+++ src/main/java/org/fastcampus/user/ui/UserController.java
@@ -3,12 +3,13 @@ package org.fastcampus.user.ui;
 import lombok.RequiredArgsConstructor;
 import org.fastcampus.common.ui.Response;
 import org.fastcampus.user.application.UserService;
+import org.fastcampus.user.application.dto.GetUserListResponseDto;
 import org.fastcampus.user.application.dto.UserCreateRequestDto;
 import org.fastcampus.user.domain.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.fastcampus.user.repository.jpa.JpaUserListQueryRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -16,10 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-
+    private final JpaUserListQueryRepository userListQueryRepository;
     @PostMapping
     public Response<Long> createUser(@RequestBody UserCreateRequestDto dto){
         User user = userService.createUser(dto);
         return Response.ok(user.getId());
     }
+
+    @GetMapping("/{userId}/follower")
+    public Response<List<GetUserListResponseDto>> getFollowerList(@PathVariable(name = "userId") Long userId){
+        List<GetUserListResponseDto> result = userListQueryRepository.getFollowerUserList(userId);
+        return Response.ok(result);
+    }
+
+    @GetMapping("/{userId}/following")
+    public Response<List<GetUserListResponseDto>> getFollowingList(@PathVariable(name = "userId") Long userId){
+        List<GetUserListResponseDto> result = userListQueryRepository.getFollowingUserList(userId);
+        return Response.ok(result);
+    }
+
 }

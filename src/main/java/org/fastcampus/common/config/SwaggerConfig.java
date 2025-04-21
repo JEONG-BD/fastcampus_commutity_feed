b6f84d1a0b1b7439d9ac9c35da63b23477913fa7
JEONG-BD <dev.byeongdu@gmail.com>
2025-04-21 18:12:38 +0900

    SwaggerConfig 생성

---
+++ src/main/java/org/fastcampus/common/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package org.fastcampus.common.config;
+
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi(){
+        String [] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("Open API ver 0.1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
